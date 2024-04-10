bffdb5a48e2d9f820e34f864566dce5124a9cd2e
K-a-y-a <98144859+K-a-y-a@users.noreply.github.com>
2024-04-10 23:51:02 +0800

    Create PostSessionLocal.java

---
+++ backend/ebs/ebs-ejb/src/java/session/PostSessionLocal.java
@@ -0,0 +1,33 @@
+package session;
+
+import entity.Student;
+import entity.Post;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import util.exception.PostNotFoundException;
+import util.exception.StudentNotFoundException;
+
+/**
+ *
+ * @author kaavya
+ */
+public interface PostSessionLocal {
+
+public void createPost(String postDescription, ArrayList<String> tags, ArrayList<String> imageURL);
+
+public void deletePost(Long postId) throws PostNotFoundException;
+
+public void updatePost(Long postId) throws PostNotFoundException;
+
+public void sharePost(Long postId) throws PostNotFoundException;
+
+public void likePost(Long postId)throws PostNotFoundException;
+
+public Post retrievePostById(Long postId) throws PostNotFoundException;
+
+}
