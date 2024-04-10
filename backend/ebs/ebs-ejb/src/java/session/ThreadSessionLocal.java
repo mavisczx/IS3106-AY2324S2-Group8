b8f0514027f879b1e8d27be9a2a50bd23b2a9739
K-a-y-a <98144859+K-a-y-a@users.noreply.github.com>
2024-04-10 23:53:12 +0800

    Create ThreadSessionLocal.java

---
+++ backend/ebs/ebs-ejb/src/java/session/ThreadSessionLocal.java
@@ -0,0 +1,30 @@
+package session;
+import entity.Thread;
+import entity.Student;
+import java.util.ArrayList;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import util.exception.ThreadNotFoundException;
+import util.exception.StudentNotFoundException;
+ 
+/**
+ *
+ * @author kaavya
+ */
+
+public interface ThreadSessionLocal {
+    public void createThread(String title, String desc, ArrayList<String> tags);
+      
+      public void deleteThread(Long threadId) throws ThreadNotFoundException;
+
+      public void updateThread(Long threadId) throws ThreadNotFoundException;
+    
+      public void shareThread(Long threadId) throws ThreadNotFoundException;
+      
+      public Thread retrieveThreadById(Long threadId) throws ThreadNotFoundException;
+
+      public Student retrieveStudentByThread(Long threadId) throws ThreadNotFoundException ;
+}
