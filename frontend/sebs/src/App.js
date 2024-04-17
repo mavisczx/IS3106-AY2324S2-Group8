f2ea6c9082dc20ff4bb68dfe1b8a418d73d06dc5
mavischeng <mavischeng74@gmail.com>
2024-04-17 15:03:46 +0800

    updated the routing for login and register, tidy app.js

--- frontend/sebs/src/App.js
+++ frontend/sebs/src/App.js
@@ -1,29 +1,27 @@
-import { Routes, Route } from "react-router-dom";
-import './App.css';
-import Sidebar from './components/Sidebar';
-
-import Index from "./containers/Index";
-
-
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
-import { Icon } from "@iconify/react";
 import { useEffect, useState } from "react";
-import {useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Icon } from "@iconify/react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Index from "./containers/Index";
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
 
-
   return (
     <div className="bg-stone-300 min-h-screen min-v-screen flex">
-      <Sidebar loggedIn = {loggedIn}
-      setLoggedIn = {setLoggedIn}/>
-      <div className = "content-wrapper w-full m-10">
-          <Routes>
+      <Sidebar loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+      <div className="content-wrapper w-full m-10">
+        <Routes>
+          <Route path="/index" element={<Index />} />
+          <Route path="/login" element={<Index />} />
+          <Route path="/register" element={<Index />} />
 
-            <Route path = "/" element = {<Index/>}/>
-          </Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
       </div>
     </div>
   );
