f10f09915b8cf7cd772041bdddd2c8e6e762d0ec
K-a-y-a <98144859+K-a-y-a@users.noreply.github.com>
2024-04-17 15:14:43 +0800

    Update PostSession.java

--- backend/ebs/ebs-ejb/src/java/session/PostSession.java
+++ backend/ebs/ebs-ejb/src/java/session/PostSession.java
@@ -4,12 +4,14 @@ import entity.Post;
 import entity.Student;
 import java.util.ArrayList;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import util.exception.PostNotFoundException;
+import util.exception.StudentNotFoundException;
 
 /**
  *
@@ -20,52 +22,82 @@ import util.exception.PostNotFoundException;
 public class PostSession implements PostSessionLocal{
 @PersistenceContext
 private EntityManager em;
+
+    @EJB
+    private StudentSessionLocal studentSessionLocal;
     
 @Override
-public void createPost(String postDescription, ArrayList<String> tags, ArrayList<String> imageURL) {
-  Post postToCreate = new Post(postDescription, tags, imageURL);
-  em.persist(postToCreate);
+public void createPost(Long studentId, String postDescription, ArrayList<String> tags, ArrayList<String> imageURL) throws StudentNotFoundException {
+   Student student = studentSessionLocal.retrieveStudentById(studentId);
+   
+    Post post = new Post(postDescription, tags, imageURL);
+    student.getPostsCreated().add(post);
+  em.persist(post);
 }
 
 @Override
 public void deletePost(Long postId) throws PostNotFoundException{
+    
    Post post = retrievePostById(postId);
+   
    em.remove(post);
 }
 
 @Override
 public void updatePost(Long postId) throws PostNotFoundException {
    Post post = retrievePostById(postId);
+   
         post.setPostDescription(post.getPostDescription());
         post.setTags(post.getTags());
         post.setImageURL(post.getImageURL());
+        
 }
 
 @Override
 public void sharePost(Long postId) throws PostNotFoundException {
+    
     Post post = retrievePostById(postId);
+    
     int shareCount = post.getShareCount();
     shareCount++;
+    
     post.setShareCount(shareCount);
-    // figure out how to acutally send the 'msg'
+    
+    // figure out how to actually send the 'msg'
 
 }
 @Override
 public void likePost(Long postId) throws PostNotFoundException {
+    
     Post post = retrievePostById(postId);
+    
     int likeCount = post.getLikeCount();
     likeCount++;
+    
     post.setLikeCount(likeCount);
 }
 
 @Override
 public Post retrievePostById(Long postId) throws PostNotFoundException {
+    
   Post post = em.find(Post.class, postId);
+  
   if (post != null) {
     return post;
   } else {
        throw new PostNotFoundException("Error: Post does not Exist!");
            }
   } 
+
+@Override
+ public Boolean isPostReported(Long postId) throws PostNotFoundException {
+     
+     Post post = retrievePostById(postId);
+     return post.isIfReported();
+     //just to check whether post has been reported by any other Student
+ }
+ 
+ 
+ 
 
 }
