eab84f132b79ef8701c9b97d4c93ecbef0ff8254
K-a-y-a <98144859+K-a-y-a@users.noreply.github.com>
2024-04-18 20:36:11 +0800

    Update Landing.js

--- frontend/sebs/src/containers/Landing.js
+++ frontend/sebs/src/containers/Landing.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
+
 //import Api from "../helpers/API";
 import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 import Button from "../components/Button";
 import CustomDialog from "../components/CustomDialog";
@@ -12,17 +13,26 @@ import Searchbar from "./Searchbar/Searchbar";
 import Timeline from "./Timeline/Timeline";
 import { Icon } from "@iconify/react";
 
-function Landing() {
-  return (
+import { Link } from "react-router-dom";
+
+   
+function Landing(){
+return (
+
     <div className="landing">
-      <div className="landing__navWraper">
-        <Searchbar />
-      </div>
-      <div className="landing__timeline">
-        <Timeline />
-      </div>
+        <div className="landing__navWraper">
+            <Searchbar />
+            <div className= "swapToThreads">
+            <Link to="/ThreadLanding" className="text-orange-600 hover:text-orange-800"> View Threads </Link>
+            </div>
+        </div>
+        <div className="landing__timeline">
+            <Timeline />
+        </div>
     </div>
-  );
+)
+
 }
+
 
 export default Landing;
