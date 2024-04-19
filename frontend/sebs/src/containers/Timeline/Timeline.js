1e33a69130e327d7a78643cfea45a8735a9e5adc
yx <chewyanxin@gmail.com>
2024-04-19 18:28:44 +0800

    api

--- frontend/sebs/src/containers/Timeline/Timeline.js
+++ frontend/sebs/src/containers/Timeline/Timeline.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import ApiThread from '../../helpers/ApiThread';
-import Thread from '../Thread/Thread';
+import React, { useState, useEffect } from "react";
+import ApiThread from "../../helpers/ApiThread";
+import Thread from "../Thread/Thread";
 
 function Timeline() {
   const [threads, setThreads] = useState([]);
@@ -8,8 +8,15 @@ function Timeline() {
 
   useEffect(() => {
     // Fetch all threads from the API
-    ApiThread.getAllThread().then((response) => {
-      setThreads(response.data);
+    const token = localStorage.getItem("token");
+    ApiThread.getThreads(token).then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          setThreads(data);
+        });
+      } else {
+        console.error("Failed to fetch threads");
+      }
     });
   }, []);
 
@@ -23,33 +30,26 @@ function Timeline() {
 
   return (
     <div className="bg-gray-900 text-white">
-    
-        
-          
-        <div className="space-y-4">
-          {threads.map((thread) => (
-            <div key={thread.id}>
-              <div
-                className="bg-gray-800 rounded-lg shadow-md p-2 cursor-pointer "
-                onClick={() => toggleThread(thread.id)}
-              >
-                <div className="flex items-center space-x-2">
-                  <div className="bg-orange-500 rounded-full w-2 h-2 flex items-center justify-center">
-                    <i className="fas fa-comment-alt text-white"></i>
-                  </div>
-                  <h2 className="text-lg font-bold">{thread.title}</h2>
+      <div className="space-y-4">
+        {threads.map((thread) => (
+          <div key={thread.id}>
+            <div
+              className="bg-gray-800 rounded-lg shadow-md p-2 cursor-pointer "
+              onClick={() => toggleThread(thread.id)}
+            >
+              <div className="flex items-center space-x-2">
+                <div className="bg-orange-500 rounded-full w-2 h-2 flex items-center justify-center">
+                  <i className="fas fa-comment-alt text-white"></i>
                 </div>
-                <p className="text-gray-400 mt-2">{thread.description}</p>
+                <h2 className="text-lg font-bold">{thread.title}</h2>
               </div>
-              {selectedThread === thread.id && (
-                <Thread thread={thread} />
-              )}
-
+              <p className="text-gray-400 mt-2">{thread.description}</p>
             </div>
-          ))}
-        </div>
+            {selectedThread === thread.id && <Thread thread={thread} />}
+          </div>
+        ))}
       </div>
-  
+    </div>
   );
 }
 
