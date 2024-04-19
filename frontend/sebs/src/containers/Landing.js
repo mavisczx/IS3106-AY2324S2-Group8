93571241ac219d85a8d9f75e567c3bec2de5fc50
K-a-y-a <98144859+K-a-y-a@users.noreply.github.com>
2024-04-19 22:29:59 +0800

    Update Landing.js

--- frontend/sebs/src/containers/Landing.js
+++ frontend/sebs/src/containers/Landing.js
@@ -14,18 +14,9 @@ import LandingBg from "./gradient.jpg";
 function Landing() {
   return (
     <div className="container mx-auto py-8">
-      <div className="flex flex-col">
-        <div className="bg-white rounded-lg shadow-md p-4 mb-4 border-4 border-gray-400 border-solid" style={{ backgroundImage: `url(${LandingBg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <div className="bg-transparent rounded-lg shadow-md p-4 mb-4 border-4 border-orange-500 border-solid relative">
-            <Searchbar />
-          </div>
-        </div>
-        <div className="border-t border-orange-500 my-4 relative">
-          <div className="absolute top-0 left-0 right-0 h-2 bg-gradient-to-r from-gray-400 to-gray-200"></div>
-        </div>
         <Timeline />
       </div>
-    </div>
+    
   );
 }
 
